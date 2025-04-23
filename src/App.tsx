b6ce54df5d943ef18c0ed5042e87d0e4353bce60
Kamila Carvalho <kamilaingridc@gmail.com>
2025-04-23 15:55:21 -0300

    :tada: Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+     <div className="container">
+        <div className="teste">
+          <p>Nome: Gustavo Henrique Moreira</p>
+        </div>
+     </div>
+    </>
+  
+  )
+}
